69e68fb3d4d16c7a36f099013015f5848cf56001
NewellWalker <newell.walker.1@gmail.com>
2023-01-17 14:29:01 -0500

    change comma formatting

--- main.js
+++ main.js
@@ -4,9 +4,9 @@
 
 const first = ['Hi', 'Hello', 'Hey', 'Howdy'];
 
-const second = ['friend', 'amigo', 'partner', 'bud'];
+const second = ['friend,', 'amigo,', 'partner,', 'bud,'];
 
-const third = [', have good day.', ', time get after it.', ', create the life you want.', ',dont fall shrot of your potential.'];
+const third = ['have good day.', 'time get after it.', 'create the life you want.', 'dont fall shrot of your potential.'];
 
 function message_gen() {
     console.log(`${first[Math.floor(Math.random() * 4)]} ${second[Math.floor(Math.random() * 4)]} ${third[Math.floor(Math.random() * 4)]}`);
