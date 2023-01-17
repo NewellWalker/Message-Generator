e8c6d8c82402d1c1ad03cc4a91060f8d28b7faa2
NewellWalker <newell.walker.1@gmail.com>
2023-01-17 13:20:09 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Code used to generate random message broken up into three parts,
+// using RNG to pick out of array of strings for beginning, middle,
+// and end
+
+const first = ['1', '2', '3', '4'];
+
+const second = ['1', '2', '3', '4'];
+
+const third = ['1', '2', '3', '4'];
+
